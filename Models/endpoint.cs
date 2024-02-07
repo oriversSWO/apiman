883972dfb7c8e306559e35447f3ab44016033306
oscar.rivera <oscar.rivera@softwareone.com>
2024-02-07 16:03:03 -0500

    implementacion de data y funciones de lista

---
+++ Models/endpoint.cs
@@ -0,0 +1,14 @@
+﻿namespace apiman.Models
+    {
+    public class endpoint
+        {
+        public string name { get; set; }
+        public string url { get; set; }
+        public string description { get; set; }
+        public bool enabled { get; set; }
+        public string[] tags { get; set; }
+        public string group { get; set; }
+        public string body { get; set; }   // Almacena JSON Schema
+        public string response { get; set; }  // Almacena JSON Schema
+        }
+    }
---
+++ Utils/Search.cs
@@ -0,0 +1,32 @@
+﻿using apiman.Models;
+using System.Text.Json;
+
+namespace apiman.Utils
+    {
+    public class Search
+        {
+        private List<endpoint> _endpoints;
+
+        public Search()
+            {
+            string jsonFilePath = "Data/endpoints.json";
+            string jsonContent = File.ReadAllText(jsonFilePath);
+            _endpoints = JsonSerializer.Deserialize<List<endpoint>>(jsonContent!);
+            }
+
+        public List<endpoint> ListByTag(string tag, bool enabled = true)
+            {
+            return _endpoints.Where(e => e.tags.Contains(tag) && e.enabled == enabled).ToList();
+            }
+
+        public List<endpoint> ListByGroup(string group, bool enabled = true)
+            {
+            return _endpoints.Where(e => e.group == group && e.enabled == enabled).ToList();
+            }
+
+        public List<endpoint> ListByEnabled(bool enabled)
+            {
+            return _endpoints.Where(e => e.enabled == enabled).ToList();
+            }
+        }
+    }
